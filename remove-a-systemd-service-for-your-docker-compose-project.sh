c77d600dfbb281655dfc570181cf77cdfb2e70a6
Eric Naud <enaud@eric-naud.de>
2023-06-21 18:53:56 +0200

    Update remove-a-systemd-service-for-your-docker-compose-project.sh

--- remove-a-systemd-service-for-your-docker-compose-project.sh
+++ remove-a-systemd-service-for-your-docker-compose-project.sh
@@ -5,14 +5,14 @@ echo "Stopping & Disabling $SERVICENAME"
 # Stop systemd service
 sudo systemctl stop $SERVICENAME.service
 # Disable  systemd service now
-sudo systemctl disable $SERVICENAME-reload.timer
-sudo systemctl disable $SERVICENAME-reload.service
+sudo systemctl disable $SERVICENAME-restart.timer
+sudo systemctl disable $SERVICENAME-restart.service
 sudo systemctl disable $SERVICENAME.service
 
 
 echo "Remove systemd services for $SERVICENAME"
 rm /etc/systemd/system/$SERVICENAME.service
-rm /etc/systemd/system/$SERVICENAME-reload.service
-rm /etc/systemd/system/$SERVICENAME-reload.timer
+rm /etc/systemd/system/$SERVICENAME-restart.service
+rm /etc/systemd/system/$SERVICENAME-restart.timer
 
 sudo systemctl daemon-reload
